f8ee52700508cc667758b7c5d62440ecb60b8a0b
verasavosina <savosinkina0110@gmail.com>
2026-05-23 21:25:26 +0300

    Update lab2.js

--- lab2.js
+++ lab2.js
@@ -1,9 +1,21 @@
 'use strict';
 
+/**
+ * Возводит число x в степень n
+ * @param {number} x - Основание степени
+ * @param {number} n - Показатель степени (целое число)
+ * @returns {number} Результат возведения в степень
+ */
 function pow(x, n) {
     return Math.pow(x, n);
 }
 
+/**
+ * Вычисляет сумму чисел от 1 до n включительно
+ * Создана с помощью синтаксиса new Function
+ * @param {number} n - Натуральное число
+ * @returns {number} Сумма чисел от 1 до n
+ */
 const sumTo = new Function('n', `
     let sum = 0;
     for (let i = 1; i <= n; i++) {
@@ -12,16 +24,32 @@ const sumTo = new Function('n', `
     return sum;
 `);
 
+/**
+ * Проверяет, является ли год високосным
+ * @param {number} year - Год для проверки
+ * @returns {boolean} true, если год високосный, иначе false
+ */
 function isLeapYear(year) {
     return (year % 400 === 0) || (year % 4 === 0 && year % 100 !== 0);
 }
 
+/**
+ * Вычисляет факториал числа n (n!)
+ * Использует рекурсивный вызов
+ * @param {number} n - Число для вычисления факториала
+ * @returns {bigint} Факториал числа в формате BigInt
+ */
 function factorial(n) {
     if (n < 0) return 0n;
     if (n === 0 || n === 1) return 1n;
     return BigInt(n) * factorial(n - 1);
 }
 
+/**
+ * Возвращает n-е число Фибоначчи
+ * @param {number} n - Порядковый номер числа Фибоначчи
+ * @returns {bigint} n-е число Фибоначчи в формате BigInt
+ */
 function fib(n) {
     if (n < 0) return 0n;
     if (n === 0) return 0n;
@@ -39,6 +67,14 @@ function fib(n) {
     return curr;
 }
 
+/**
+ * Создаёт функцию сравнения с заданным значением x
+ * @param {number} x - Базовое значение для сравнения
+ * @returns {Function} Анонимная функция, принимающая y и возвращающая:
+ *                     true, если y > x
+ *                     false, если y < x
+ *                     null, если y === x
+ */
 function compare(x) {
     return function(y) {
         if (y > x) return true;
@@ -47,6 +83,11 @@ function compare(x) {
     };
 }
 
+/**
+ * Вычисляет сумму всех переданных аргументов
+ * @param {...number} args - Числа для суммирования
+ * @returns {number} Сумма всех аргументов
+ */
 function sum(...args) {
     let total = 0;
     for (let i = 0; i < args.length; i++) {
@@ -55,6 +96,11 @@ function sum(...args) {
     return total;
 }
 
+/**
+ * Добавляет к объекту символьное свойство blackSpot = true из глобального реестра
+ * @param {Object} obj - Целевой объект
+ * @returns {Object} Тот же объект с добавленным символьным свойством
+ */
 function addBlackSpot(obj) {
     const blackSpotSymbol = Symbol.for("blackSpot");
     obj[blackSpotSymbol] = true;
